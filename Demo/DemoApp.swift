761123056d08f2341df8bc12c4439ac0e16aa8a6
bhagyashree.modak <bhagyashree.modak@sts.in>
2023-11-29 12:12:51 +0530

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Bhagyashree Modak on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
